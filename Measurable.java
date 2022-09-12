b7d5f3517634af2da88836f4ce83b2253efee742
PDacey <98864045+PDacey@users.noreply.github.com>
2022-09-12 17:06:17 -0400

    Add files via upload

---
+++ Measurable.java
@@ -0,0 +1,15 @@
+/**
+ * @author Paul Dacey
+ */
+
+/**
+   Describes any class whose objects can be measured.
+*/
+public interface Measurable
+{
+   /**
+      Computes the measure of the object.
+      @return the measure
+   */
+   double getMeasure();
+}
